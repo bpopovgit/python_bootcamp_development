b3cef2b8bed8a5290bae4dbe60e5364b2fc26299
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2024-08-17 22:56:07 +0300

    fixed bugs, game is ready to play

--- blackjack_game/main.py
+++ blackjack_game/main.py
@@ -23,45 +23,93 @@ def calculate_player_score_after_player_choice(user_cards_, user_score_):
     return user_score_
 
 
-def check_if_user_lost(user_score_, computer_score_):
-    if user_score_ > 21 > computer_score_:
-        return "You went over. You lose :("
+def add_to_computer_cards(computer_cards_list, computer_score_):
+    while computer_score_ < 16:
+        computer_cards_list.append(random.randint(1, 11))
+    return computer_cards_list
 
 
-while not is_game_over:
-    user_cards.append(random.randint(1, 11))
-    user_cards.append(random.randint(1, 11))
+def calculate_computer_score(computer_cards_, computer_score_):
+    computer_score_ += computer_cards_[-1]
+    return computer_score_
 
-    user_score += sum(user_cards)
 
-    computer_cards.append(random.randint(1, 11))
-    computer_cards.append(random.randint(1, 11))
+# def check_result(user_score_, computer_score_):
+#     if computer_score_ == blackjack_score:
+#         return "You lose!"
+#     if user_score_ == blackjack_score and computer_score_ != blackjack_score:
+#         return "You win! :)"
+#     if computer_score_ > 21:
+#         return "You win!"
+#     if user_score > 21:
+#         return "You lose!"
 
-    computer_score += sum(computer_cards)
+def score_checker(user_score_, computer_score_):
+    if computer_score_ == blackjack_score:
+        return "Computer wins!"
+    if user_score_ == blackjack_score and computer_score_ != blackjack_score:
+        return "You win!"
+    if user_score_ > 21:
+        return "You went over! You lose!"
+    if computer_score_ > 21:
+        return "Computer went over! You win."
+    if 21 > user_score_ == computer_score_ < 21:
+        return f"Draw!"
 
-    print(f"Your cards: {user_cards}, current score: {user_score}")
+
+def final_stats_output():
+    return (f"Your final hand: {user_cards}, final score: {user_score}"
+            f"\nComputer's final hand: {computer_cards}, final score: {computer_score}")
+
+
+# while not is_game_over:
+#     user_cards.append(random.randint(1, 11))
+#     user_cards.append(random.randint(1, 11))
+#
+#     user_score += sum(user_cards)
+#
+#     computer_cards.append(random.randint(1, 11))
+#     computer_cards.append(random.randint(1, 11))
+#
+#     computer_score += sum(computer_cards)
+#
+#     print(f"Your cards: {user_cards}, current score: {user_score}")
+#     print(f"Computer's first card: {computer_cards[0]}")
+#
+#     player_choice = input("Type 'y' to get another card, type 'n' to pass: ")
+
+user_cards.append(random.randint(1, 11))
+user_cards.append(random.randint(1, 11))
+
+user_score += sum(user_cards)
+
+computer_cards.append(random.randint(1, 11))
+computer_cards.append(random.randint(1, 11))
+
+computer_score += sum(computer_cards)
+
+print(f"Your cards: {user_cards}, current score: {user_score}")
+print(f"Computer's first card: {computer_cards[0]}")
+
+player_choice = input("Type 'y' to get another card, type 'n' to pass: ")
+
+while player_choice == 'y' and not is_game_over:
+    updated_user_cards_list = add_to_user_cards(user_cards)
+    user_score = calculate_player_score_after_player_choice(updated_user_cards_list, user_score)
+    print(f"Your cards: {updated_user_cards_list},"
+          f" current score: {user_score}")
     print(f"Computer's first card: {computer_cards[0]}")
 
-    player_choice = input("Type 'y' to get another card, type 'n' to pass: ")
+    if user_score == blackjack_score or user_score > 21:
+        print(score_checker(user_score, computer_score))
+        print(final_stats_output())
+        is_game_over = True
 
-    while player_choice == 'y':
-        updated_user_card_list = add_to_user_cards(user_cards)
-        user_score = calculate_player_score_after_player_choice(updated_user_card_list, user_score)
-        print(f"Your cards: {updated_user_card_list},"
-              f" current score: {user_score}")
-        print(f"Computer's first card: {computer_cards[0]}")
-        if check_if_user_lost(user_score, computer_score):
-            is_game_over = True
+    player_choice = input("Type 'y' to get another card, type 'n' to pass: ") if not is_game_over else exit()
 
-        # print(check_score(user_score, computer_score))
-
-        player_choice = input("Type 'y' to get another card, type 'n' to pass: ")
-
-    if player_choice == 'n':
-        while computer_score < 16:
-            computer_cards.append(random.randint(1, 11))
-            computer_score += computer_cards[-1]
-        # print(check_score(user_score, computer_score))
-
+if player_choice == 'n' and not is_game_over:
+    updated_computer_cards_list = add_to_computer_cards(computer_cards, computer_score)
+    computer_score = calculate_computer_score(updated_computer_cards_list, computer_score)
+    print(final_stats_output())
+    print(score_checker(user_score, computer_score))
     is_game_over = True
-    # print(user_cards, computer_cards)
