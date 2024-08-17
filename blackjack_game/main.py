d42a0e8a4ed369322058440576e09856e98ea3e8
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2024-08-17 13:29:01 +0300

    add second version of blackjack, still bugs to fix

--- blackjack_game/main.py
+++ blackjack_game/main.py
@@ -23,18 +23,9 @@ def calculate_player_score_after_player_choice(user_cards_, user_score_):
     return user_score_
 
 
-def check_score(user_score_, computer_score_):
+def check_if_user_lost(user_score_, computer_score_):
     if user_score_ > 21 > computer_score_:
         return "You went over. You lose :("
-    elif user_score_ == 21 and computer_score_ == 21:
-        return "Draw!"
-    elif user_score_ < 21 and computer_score_ < 21:
-        if user_score_ > computer_score_:
-            return "You win!"
-    elif user_score_ == 21 and computer_score_ < 21:
-        return "You got a blackjack! You win!"
-    elif computer_score_ == 21 and user_score_ < 21:
-        return "Computer got a blackjack! You lose!"
 
 
 while not is_game_over:
@@ -55,10 +46,14 @@ while not is_game_over:
 
     while player_choice == 'y':
         updated_user_card_list = add_to_user_cards(user_cards)
+        user_score = calculate_player_score_after_player_choice(updated_user_card_list, user_score)
         print(f"Your cards: {updated_user_card_list},"
-              f" current score: {calculate_player_score_after_player_choice(user_cards, user_score)}")
+              f" current score: {user_score}")
         print(f"Computer's first card: {computer_cards[0]}")
-        print(check_score(user_score, computer_score))
+        if check_if_user_lost(user_score, computer_score):
+            is_game_over = True
+
+        # print(check_score(user_score, computer_score))
 
         player_choice = input("Type 'y' to get another card, type 'n' to pass: ")
 
@@ -66,7 +61,7 @@ while not is_game_over:
         while computer_score < 16:
             computer_cards.append(random.randint(1, 11))
             computer_score += computer_cards[-1]
-        print(check_score(user_score, computer_score))
+        # print(check_score(user_score, computer_score))
 
     is_game_over = True
     # print(user_cards, computer_cards)
